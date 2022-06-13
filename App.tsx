419bb208a50029aba10440e4e94a6d2e59644257
Priyanshu Panwar <priyanshu009ch@gmail.com>
2022-06-13 07:58:51 +0000

    Basic Components Testing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Footer />
       <StatusBar style="auto" />
     </View>
   );
